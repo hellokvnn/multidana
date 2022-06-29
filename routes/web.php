69eace565c6c9eaddf5f7f0dd62858eb72100f85
hellokvnn <herconstellation@gmail.com>
2022-06-29 23:55:43 +0700

    admin page pulsa retail

--- routes/web.php
+++ routes/web.php
@@ -48,3 +48,74 @@ Route::get('/kontak-kami', function () {
         "title" => "Kontak Kami"
     ]);
 });
+
+Route::get('/dashboard', function () {
+    return view('dashboard', [
+        "title" => "Dashboard"
+    ]);
+});
+
+Route::get('/slider', function () {
+    return view('slider', [
+        "title" => "Slider"
+    ]);
+});
+
+Route::get('/pulsa-retail-setting', function () {
+    return view('pulsa-retail-setting', [
+        "title" => "Pulsa Retail Setting"
+    ]);
+});
+
+Route::get('/pulsa-telepon-setting', function () {
+    return view('pulsa-telepon-setting', [
+        "title" => "Pulsa Telepon Setting"
+    ]);
+});
+
+Route::get('/indosat-retail', function () {
+    return view('indosat-retail', [
+        "title" => "Indosat Retail"
+    ]);
+});
+
+Route::get('/telkomsel-retail', function () {
+    return view('telkomsel-retail', [
+        "title" => "Telkomsel Retail"
+    ]);
+});
+Route::get('/axis-retail', function () {
+    return view('axis-retail', [
+        "title" => "Axis Retail"
+    ]);
+});
+
+Route::get('/xl-retail', function () {
+    return view('xl-retail', [
+        "title" => "XL Retail"
+    ]);
+});
+
+Route::get('/smartfren-retail', function () {
+    return view('smartfren-retail', [
+        "title" => "Smartfren Retail"
+    ]);
+});
+
+Route::get('/three-retail', function () {
+    return view('three-retail', [
+        "title" => "Three Retail"
+    ]);
+});
+
+Route::get('/mtix-retail', function () {
+    return view('mtix-retail', [
+        "title" => "MTIX Retail"
+    ]);
+});
+
+Route::get('/tix-retail', function () {
+    return view('tix-retail', [
+        "title" => "TIX Retail"
+    ]);
+});
